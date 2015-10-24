269b4f57fa28794a30f8d8b0cefe7db36ff5e554
nuriyevn <nuriyevn@gmail.com>
2015-10-24 23:01:50 +0300

    mysql setup added, setup know issues added, ejudge start script proto added

---
+++ full_cmmisr.sh
@@ -0,0 +1,2 @@
+su -c "/opt/ejudge/bin/ejudge-control stop" root
+su -c "/opt/ejudge/bin/ejudge-control start" ejudge
---
+++ mysql_setup.sql
@@ -0,0 +1,18 @@
+CREATE USER 'ejudge'@'%' IDENTIFIED BY '*******';
+GRANT USAGE ON *.* TO 'ejudge'@'%' IDENTIFIED BY '*******' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+CREATE DATABASE IF NOT EXISTS `ejudge`;
+GRANT ALL PRIVILEGES ON `ejudge`.* TO 'ejudge'@'%';
+
+//Check that mysql database password in judges folder is the same as in all queries
+// Probably ejudge-setup works incorrectly
+GRANT ALL PRIVILEGES ON *.* TO 'ejudge'@'localhost';
+
+
+// Useful commands
+
+USE mysql
+DESCRIBE mysql.user
+GRANT USAGE ON *.* TO 'ejudge'@'%' IDENTIFIED BY '*******' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+SELECT user, host, password FROM mysql.user WHERE 1;
+
+
